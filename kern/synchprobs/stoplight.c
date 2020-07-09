37ca7057f4e9391dee6687a1c87282ab74e1c75c
Github22799 <31520065+Github22799@users.noreply.github.com>
2020-07-09 10:28:47 +0200

    fixed some typos

--- kern/synchprobs/stoplight.c
+++ kern/synchprobs/stoplight.c
@@ -28,7 +28,7 @@
  */
 
 /*
- * Driver code is in kern/tests/synchprobs.c We will replace that file. This
+ * Driver code is in kern/test/synchprobs.c We will replace that file. This
  * file is yours to modify as you see fit.
  *
  * You should implement your solution to the stoplight problem below. The
@@ -55,7 +55,7 @@
  * leaveIntersection().
  *
  * You will probably want to write some helper functions to assist with the
- * mappings. Modular arithmetic can help, e.g. a car passing straight through
+ * mappings. Modular arithmetic can help, e.g. a car turning left through
  * the intersection entering from direction X will leave to direction (X + 2)
  * % 4 and pass through quadrants X and (X + 3) % 4.  Boo-yah.
  *
